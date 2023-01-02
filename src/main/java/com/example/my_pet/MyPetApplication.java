60836568c7720b4244ba98ee588dbbeb65ab78c4
jawadDouf <jdoufare2001@gmail.com>
2023-01-02 11:01:46 +0100

    Build the project structure

---
+++ src/main/java/com/example/my_pet/MyPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_pet/MyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
